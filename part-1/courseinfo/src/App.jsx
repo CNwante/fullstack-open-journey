47d52f7bc4e2681e8b9252870aea50006b21260f
Solomon Nwante <solomonchuks202@gmail.com>
2026-07-18 09:53:44 +0100

    refactor(app): group course and its parts into a single object
    - Update the breaking components to match the changes.
    
    Learning:
    - Strengthen my uderstanding of how to access objects and arrays data.

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -1,37 +1,48 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
 
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h1>{props.course.name}</h1>
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part
+        part={props.course.parts[0].name}
+        exercises={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercises={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
     </div>
   )
 }
@@ -45,7 +56,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  const totalExercises = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <p>Number of exercises {totalExercises}</p>
   )
